bf2e0da46f503893a5803bd3ad87a14b6c8aa197
benoitguigal <benoit.guigal@gmail.com>
2016-02-13 12:16:51 +0100

    better tests

--- tests.py
+++ tests.py
@@ -7,6 +7,14 @@ from ticketrenderer import TicketRenderer
 class TestTicketRenderer(unittest.TestCase):
 
     def setUp(self):
+        self.media_url = 'http://media'
+        self.css_url = 'http://static/ticket.css'
+
+    def test_render(self):
+        """
+        TicketRenderer should render a ticket
+        """
+
         html = '{{picture}} {{code}} {{datetime | datetimeformat}} ' \
                '{{textvariable_1}} {{imagevariable_2}} ' \
                '{{image_3}}'
@@ -24,11 +32,9 @@ class TestTicketRenderer(unittest.TestCase):
                 'name': 'image2'
             }
         ]
-
         image_variables = [{'id': '2', 'items': items}]
         images = [{'id': '3', 'name': 'image3'}]
-
-        self.template = {
+        template = {
             'html': html,
             'images': images,
             'image_variables': image_variables,
@@ -36,23 +42,58 @@ class TestTicketRenderer(unittest.TestCase):
             'title': 'title',
             'subtitle': 'subtitle'
         }
-
-        self.media_url = 'http://media'
-        self.css_url = 'http://static/ticket.css'
-        self.ticket_renderer = TicketRenderer(self.template, self.media_url, self.css_url)
-
-
-    def test_render(self):
+        ticket_renderer = TicketRenderer(template, self.media_url, self.css_url)
         code = 'SJ98H'
         date = datetime(2016, 01, 01)
         picture = 'http://path/to/picture'
-        rendered = self.ticket_renderer.render(code=code, date=date, picture=picture)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture)
         self.assertIn("http://path/to/picture", rendered)
         self.assertIn(code, rendered)
         self.assertIn("http://static/ticket.css", rendered)
         self.assertTrue("Titi" in rendered or "Vicky" in rendered or "Benni" in rendered)
         self.assertTrue("http://media/images/image1" in rendered or "http://media/images/image2" in rendered)
         self.assertTrue("http://media/images/image3" in rendered)
+
+    def test_set_date_format(self):
+        """
+        Ticket renderer should handle datetimeformat filter
+        """
+        html = '{{datetime | datetimeformat("%Y/%m/%d")}}'
+        template = {
+            'html': html,
+            'images': [],
+            'image_variables': [],
+            'text_variables': [],
+            'title': '',
+            'subtitle': ''
+        }
+        ticket_renderer = TicketRenderer(template, self.media_url, self.css_url)
+        code = 'SJ98H'
+        date = datetime(2010, 01, 01)
+        picture = 'http://path/to/picture'
+        rendered_html = ticket_renderer.render(code=code, date=date, picture=picture)
+        assert "2010/01/01" in rendered_html
+
+
+    def test_encode_non_unicode_character(self):
+        """
+        Ticket renderer should encode non unicode character
+        """
+        html = u"Du texte avec un accent ici: é"
+        template = {
+            'html': html,
+            'images': [],
+            'image_variables': [],
+            'text_variables': [],
+            'title': '',
+            'subtitle': ''
+        }
+        ticket_renderer = TicketRenderer(template, self.media_url, self.css_url)
+        code = 'SJ98H'
+        date = datetime(2010, 01, 01)
+        picture = 'http://path/to/picture'
+        rendered_html = ticket_renderer.render(code=code, date=date, picture=picture)
+        assert u'Du texte avec un accent ici: é' in rendered_html
 
 
 if __name__ == '__main__':
