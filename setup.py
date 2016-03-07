a4a26f3562642f3fc43a032442b4cac839cf369a
benoitguigal <benoit.guigal@gmail.com>
2016-03-07 14:16:39 +0100

    bump to version 0.1.9

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ def read(fname):
 setup(
     name = "ticketrenderer",
     packages = ["ticketrenderer"],
-    version = "0.1.8",
+    version = "0.1.9",
     author = "Benoît Guigal",
     author_email = "benoit@postcardgroup.com",
     description = ("A library used to render Figure tickets from ticket templates"),
     url = "https://github.com/Postcard/ticket-renderer-python",
-    download_url = 'https://github.com/Postcard/ticket-renderer-python/tarball/0.1.8',
+    download_url = 'https://github.com/Postcard/ticket-renderer-python/tarball/0.1.9',
     py_modules=('ticketrenderer',),
     install_requires=[
         'jinja2==2.7.3',
