eb421928c501565a9011b1835d1b655e3cd825b7
Benoît Guigal <benoit.guigal@gmail.com>
2016-07-08 14:16:16 +0200

    text and image variables items can be rendered sequentially (#1)
    bump to version 0.2.0

--- ticketrenderer/tests.py
+++ ticketrenderer/tests.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import unittest
 from datetime import datetime
 
@@ -19,7 +21,7 @@ class TestTicketRenderer(unittest.TestCase):
                '{{textvariable_1}} {{imagevariable_2}} ' \
                '{{image_3}}'
         texts = [{'text':'Titi'}, {'text':'Vicky'}, {'text':'Benni'}]
-        text_variables = [{'id': '1', 'items': texts}]
+        text_variables = [{'id': '1', 'items': texts, 'mode': 'random'}]
 
         items = [
             {
@@ -32,7 +34,7 @@ class TestTicketRenderer(unittest.TestCase):
                 'name': 'image2'
             }
         ]
-        image_variables = [{'id': '2', 'items': items}]
+        image_variables = [{'id': '2', 'items': items, 'mode': 'random'}]
         images = [{'id': '3', 'name': 'image3'}]
         template = {
             'html': html,
@@ -46,7 +48,7 @@ class TestTicketRenderer(unittest.TestCase):
         code = 'SJ98H'
         date = datetime(2016, 01, 01)
         picture = 'http://path/to/picture'
-        rendered = ticket_renderer.render(code=code, date=date, picture=picture)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=0)
         self.assertIn("http://path/to/picture", rendered)
         self.assertIn(code, rendered)
         self.assertIn("http://static/ticket.css", rendered)
@@ -54,6 +56,58 @@ class TestTicketRenderer(unittest.TestCase):
         self.assertTrue("http://media/images/image1" in rendered or "http://media/images/image2" in rendered)
         self.assertTrue("http://media/images/image3" in rendered)
         self.assertIn("01/01/2016 00:00", rendered)
+
+    def test_render_sequential(self):
+        html = '{{css_url}} {{picture}} {{code}} {{datetime | datetimeformat}} ' \
+               '{{textvariable_1}} {{imagevariable_2}} ' \
+               '{{image_3}}'
+        texts = [{'text':'Titi'}, {'text':'Vicky'}, {'text':'Benni'}]
+        text_variables = [{'id': '1', 'items': texts, 'mode': 'sequential'}]
+
+        items = [
+            {
+                'id': '1',
+                'name': 'image1'
+
+            },
+            {
+                'id': '2',
+                'name': 'image2'
+            }
+        ]
+        image_variables = [{'id': '2', 'items': items, 'mode': 'sequential'}]
+        images = [{'id': '3', 'name': 'image3'}]
+        template = {
+            'html': html,
+            'images': images,
+            'image_variables': image_variables,
+            'text_variables': text_variables,
+            'title': 'title',
+            'description': 'description'
+        }
+        ticket_renderer = TicketRenderer(template, self.media_url, self.css_url)
+        code = 'SJ98H'
+        date = datetime(2016, 01, 01)
+        picture = 'http://path/to/picture'
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=0)
+        self.assertIn('Titi', rendered)
+        self.assertIn('image1', rendered)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=1)
+        self.assertIn('Vicky', rendered)
+        self.assertIn('image2', rendered)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=2)
+        self.assertIn('Benni', rendered)
+        self.assertIn('image1', rendered)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=3)
+        self.assertIn('Titi', rendered)
+        self.assertIn('image2', rendered)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=4)
+        self.assertIn('Vicky', rendered)
+        self.assertIn('image1', rendered)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=5)
+        self.assertIn('Benni', rendered)
+        self.assertIn('image2', rendered)
+
 
     def test_render_no_variables_no_title_no_description(self):
         """
@@ -72,7 +126,7 @@ class TestTicketRenderer(unittest.TestCase):
         code = 'SJ98H'
         date = datetime(2016, 01, 01)
         picture = 'http://path/to/picture'
-        rendered = ticket_renderer.render(code=code, date=date, picture=picture)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=0)
         self.assertIn("http://path/to/picture", rendered)
         self.assertIn(code, rendered)
         self.assertIn("http://static/ticket.css", rendered)
@@ -96,7 +150,7 @@ class TestTicketRenderer(unittest.TestCase):
         code = 'SJ98H'
         date = datetime(2016, 01, 01)
         picture = 'http://path/to/picture'
-        rendered = ticket_renderer.render(code=code, date=date, picture=picture)
+        rendered = ticket_renderer.render(code=code, date=date, picture=picture, counter=0)
         self.assertIn("http://path/to/picture", rendered)
         self.assertIn(code, rendered)
         self.assertIn("http://static/ticket.css", rendered)
@@ -118,7 +172,7 @@ class TestTicketRenderer(unittest.TestCase):
         code = 'SJ98H'
         date = datetime(2010, 01, 01)
         picture = 'http://path/to/picture'
-        rendered_html = ticket_renderer.render(code=code, date=date, picture=picture)
+        rendered_html = ticket_renderer.render(code=code, date=date, picture=picture, counter=0)
         assert "2010/01/01" in rendered_html
 
 
@@ -139,7 +193,7 @@ class TestTicketRenderer(unittest.TestCase):
         code = 'SJ98H'
         date = datetime(2010, 01, 01)
         picture = 'http://path/to/picture'
-        rendered_html = ticket_renderer.render(code=code, date=date, picture=picture)
+        rendered_html = ticket_renderer.render(code=code, date=date, picture=picture, counter=0)
         assert u'Du texte avec un accent ici: é' in rendered_html
 
 
